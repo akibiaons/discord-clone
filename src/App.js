afffb7fcf2b19feddd7b1b1904bce44b603db498
Arthur Fernandez <arthur@snooget.com>
2023-06-05 09:01:50 -0700

    deleted old logo adding new logo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./index.css";
 import { SideBar } from "./components/SideBar";
 import { Home } from "./pages/Home";
+import Checkout from "./pages/Checkout";
+import Items from "./pages/Items";
 
 function App() {
-  return (
-    <div className="flex">
-      <SideBar />
-      <div className="flex-grow">
-        <Home />
-      </div>
-    </div>
-  );
+  <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/items" element={<Items/>}/>
+        <Route path="/checkout" element={<Checkout/>}/>
+      </Routes>
+    </Router>
+  </>
 }
 
 export default App;
